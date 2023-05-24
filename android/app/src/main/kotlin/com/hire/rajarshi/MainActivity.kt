4138211e1f2a3fd03b7f71102ce58b58417cd292
Rajarshi <rajarshibz@gmail.com>
2023-05-24 19:15:27 +0530

    Initial commit - architecture

---
+++ android/app/src/main/kotlin/com/hire/rajarshi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hire.rajarshi.responsive_ui_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:responsive_ui_demo/app/app.dart';
+import 'package:responsive_ui_demo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
